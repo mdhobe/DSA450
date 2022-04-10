42e16a7c35dbba5d3ecdf92a361bd23c9190286c
Mrunal Sunil Dhobe <37969422+mdhobe@users.noreply.github.com>
2022-04-10 16:27:20 +0530

    Create Array Partition I.java

---
+++ Array Partition I.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for(int i=0;i<nums.length;i+=2){
+            if(nums[i] > nums[i+1]){
+                sum += nums[i+1];
+            }
+            else{
+                sum += nums[i];
+            }
+        }
+        return sum;
+    }
+}
